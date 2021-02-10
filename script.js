f565f3af2e926d02422c7494775f99c7bff8777f
Melanie Vanput <melanie.vanput@gmail.com>
2021-02-10 15:50:09 +0100

    add endgame and victory conditions + reset game

--- script.js
+++ script.js
@@ -1,56 +1,141 @@
 const board = (() => {
-    let arrayBoard = []    
-    const divBoard = document.getElementById('board');
-    
-    function createNewBoard () {
-        for (let i = 0; i < 9; i++) {
-            let arrayCase = "";
-            arrayBoard.push(arrayCase);
+    const divContainer = document.getElementById('board');
 
+    function initiate () {
+        _createNew()
+    }
+    
+    function reset() {
+        const caseArray = document.querySelectorAll('div.case')
+        caseArray.forEach(singleCase => {
+            divContainer.removeChild(singleCase)
+        });
+        _createNew();
+    }
+
+    function fill (index, symbol) {
+        divContainer.childNodes[index].textContent = symbol;
+    }
+
+    function endPopUp(message) {
+        //console.log(message)
+        let overlay = document.createElement('div');
+        overlay.classList.add('overlay');
+        overlay.textContent = message;
+        divContainer.appendChild(overlay);
+    }
+
+    function _createNew () {
+        for (let i = 0; i < 9; i++) {
             let newDivCase = document.createElement('div');
             newDivCase.setAttribute('id', 'pos' + i);
-            newDivCase.setAttribute('class', 'case');
+            newDivCase.classList.add('case');
             newDivCase.setAttribute('data-index', i);
-            newDivCase.textContent = arrayCase;
-            divBoard.appendChild(newDivCase)
+            newDivCase.textContent = "";
+            divContainer.appendChild(newDivCase)
         }
-
         _listenToCase()
     }
 
     function _listenToCase () {
         const caseArray = document.querySelectorAll('div.case')
-        caseArray.forEach(square => {
-            square.addEventListener('click', game.fillBoard)
-        });
-        
+        caseArray.forEach(singleCase => {
+            singleCase.addEventListener('click', game.play)
+        }); 
     }
 
     return { 
-        createNewBoard,
-        arrayBoard,
-        divBoard,
+        initiate,
+        reset,
+        fill,
+        endPopUp,
     };
 })();
 
 const game = (() => {
-    function fillBoard () {
+    let arrayBoard = Array(9).fill("");
+    
+    function play () {
         if (!this.textContent) {
-            let symbol = me.currentPlayer ? me.symbol : other.symbol;
-            let index = this.dataset.index
-            board.arrayBoard[index] = symbol;
-            board.divBoard.childNodes[index].textContent = symbol;
-            _toggleCurrentPlayer();   
+            let currentSymbol = _checkCurrentlyPlaying();
+
+            _fillBoard(this.dataset.index, currentSymbol);
+            
+            let isWon = _checkVictory(currentSymbol);
+            let isTie = _checkTie();
+            if (isWon || isTie) {
+                let message
+                if (isWon) {
+                    let theWinner = _winnerName(currentSymbol);
+                    message = `Yeah! ${theWinner} has won`;
+                    isWon = _reset(isWon);
+                } else if (isTie) {
+                    message = 'it\'s a tie!';
+                    isTie = _reset(isTie);
+                }  
+                board.endPopUp(message);          
+            } else if (!isWon && !isTie) {
+                _toggleCurrentPlayer(); 
+            }
         }
+    }
+    
+    function _fillBoard (index, symbol) {
+        arrayBoard[index] = symbol;
+        board.fill(index, symbol);
+    }
+
+    function _checkCurrentlyPlaying() {
+        let currentlyPlaying = player1.currentPlayer ? player1.symbol : player2.symbol;
+        return currentlyPlaying
     }
 
     function _toggleCurrentPlayer () {
-        me.currentPlayer ? me.currentPlayer = false : me.currentPlayer = true;
-        other.currentPlayer ? other.currentPlayer = false : other.currentPlayer = true;
+        player1.currentPlayer ? player1.currentPlayer = false : player1.currentPlayer = true;
+        player2.currentPlayer ? player2.currentPlayer = false : player2.currentPlayer = true;
+    }
+
+    function _checkVictory (currentSymbol) {
+        let MappedArrayBoard = arrayBoard.map(aCase => aCase === currentSymbol);
+        if (MappedArrayBoard[0] && MappedArrayBoard[1] && MappedArrayBoard[2]
+        || MappedArrayBoard[3] && MappedArrayBoard[4] && MappedArrayBoard[5]
+        || MappedArrayBoard[6] && MappedArrayBoard[7] && MappedArrayBoard[8]
+        || MappedArrayBoard[0] && MappedArrayBoard[3] && MappedArrayBoard[6]
+        || MappedArrayBoard[1] && MappedArrayBoard[4] && MappedArrayBoard[7]
+        || MappedArrayBoard[2] && MappedArrayBoard[5] && MappedArrayBoard[8]
+        || MappedArrayBoard[0] && MappedArrayBoard[4] && MappedArrayBoard[8]
+        || MappedArrayBoard[2] && MappedArrayBoard[4] && MappedArrayBoard[6]) {
+            return true;
+        } else {
+            return false
+        }
+    }
+
+    function _winnerName(symbol) {
+        if (player1.symbol === symbol) {
+            return player1.name
+        } else {
+            return player2.name
+        }
+    }
+
+    function _checkTie () {
+        if (arrayBoard.indexOf("") === -1) {
+            return true;
+        } else {
+            return false;
+        }
+    }
+
+    function _reset(element) {
+        element = false;
+        arrayBoard = Array(9).fill("");
+        board.reset();
+        return element
     }
 
     return {
-        fillBoard,
+        play,
     }
 })();
 
@@ -62,6 +147,7 @@ const Player = (name, symbol, currentPlayer) => {
     }
 }
 
-const me = Player('Player 1', 'X', true);
-const other = Player('Player 2', 'O', false);
-board.createNewBoard();
+const player1 = Player('Player 1', 'X', true);
+const player2 = Player('Player 2', 'O', false);
+
+window.onload = board.initiate();
